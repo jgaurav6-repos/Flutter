2f9f4ad0502649b53b7542c97309571982027c0e
Gaurav Jayasawal <jgaurav6@gmail.com>
2018-11-02 02:04:46 -0400

    test 2

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(SampleApp());
+}
 
-class MyApp extends StatelessWidget {
+class SampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome',
-      home: AppPage(),
-    );
+    return (MaterialApp(
+      title: "Test App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleStatefulApp(),
+    ));
   }
 }
 
-class AppPage extends StatefulWidget{
-  AppPage({Key key}) : super(key : key);
+class SampleStatefulApp extends StatefulWidget {
+  SampleStatefulApp({Key key}) : super(key: key);
 
   @override
-  _AppPageState createState() => _AppPageState();
+  _SampleStatefulAppState createState() => _SampleStatefulAppState();
 }
 
-class _AppPageState extends State<AppPage>{
+class _SampleStatefulAppState extends State<SampleStatefulApp> {
+  String stringToUpdate = "Initial String";
 
-  String textToShow = "I like Flutter";
-
-  void updateText() {
+  void _updateString() {
     setState(() {
-      if(textToShow == "Hello World"){
-        textToShow = "I like Flutter";
+//    stringToUpdate = (stringToUpdate == "Initial String" ? "Second String" : "Initial String");
+      if (stringToUpdate == "Initial String") {
+        stringToUpdate = "Next Value";
+      } else {
+        stringToUpdate = "Initial String";
       }
-      else
-      textToShow = "Hello World";
     });
   }
 
@@ -37,16 +42,16 @@ class _AppPageState extends State<AppPage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My App"),
+        title: Text("Appbar Text"),
       ),
-    body: Center(child: Text(textToShow)),
-    floatingActionButton: FloatingActionButton(
-        onPressed: updateText,
-        tooltip: "Update Text",
+      body: Center(
+        child: Text(stringToUpdate),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateString,
+        tooltip: "change text",
         child: Icon(Icons.update),
-    ),
+      ),
     );
   }
-
-
 }
