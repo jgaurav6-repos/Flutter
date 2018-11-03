8ec7c6d1e16777382b7471cb4b4a560b8dc597a9
Gaurav Jayasawal <jgaurav6@gmail.com>
2018-11-03 13:19:27 -0400

    random word generated

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,56 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(SampleApp());
-}
+void main() => runApp(MyApp());
 
-class SampleApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return (MaterialApp(
-      title: "Test App",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SampleStatefulApp(),
-    ));
-  }
-}
+    final wordPair = WordPair.random();
 
-class SampleStatefulApp extends StatefulWidget {
-  SampleStatefulApp({Key key}) : super(key: key);
-
-  @override
-  _SampleStatefulAppState createState() => _SampleStatefulAppState();
-}
-
-class _SampleStatefulAppState extends State<SampleStatefulApp> {
-  String stringToUpdate = "Initial String";
-
-  void _updateString() {
-    setState(() {
-//    stringToUpdate = (stringToUpdate == "Initial String" ? "Second String" : "Initial String");
-      if (stringToUpdate == "Initial String") {
-        stringToUpdate = "Next Value";
-      } else {
-        stringToUpdate = "Initial String";
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Appbar Text"),
-      ),
-      body: Center(
-        child: Text(stringToUpdate),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updateString,
-        tooltip: "change text",
-        child: Icon(Icons.update),
+    return MaterialApp(
+      title: 'Welcome to Flutter 2',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
       ),
     );
   }
