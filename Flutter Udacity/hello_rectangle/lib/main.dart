1e54bec9bbf7a95ed7a572e32dcd606bf00bf2f9
Gaurav Jayasawal <jgaurav6@gmail.com>
2018-11-06 03:00:02 -0500

    widget management ninja

--- Flutter Udacity/hello_rectangle/lib/main.dart
+++ Flutter Udacity/hello_rectangle/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,51 +9,178 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Ananda",
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange
+//          primaryColor: Color(0xFFFEAF83),
+//          primaryColorDark: Color(0xFFF67E3B),
+
+        ),
+        title: "Ananda Timer",
         home: Scaffold(
           appBar: AppBar(
-            title: Text("Ananda"),
+            title: Text("Ananda Timer"),
           ),
-          body: SingleRow(),
+          body: TimerPage(),
         ));
   }
 }
 
-class SingleRow extends StatelessWidget {
-  var _rowHeight = 80.0;
-  var _circularRadius = 10.0;
-  var _sidePaddings = 8.0;
-  var _textStyle = TextStyle(fontSize: 20.0);
+class TimerPage extends StatefulWidget {
+  @override
+  TimerPageState createState() => new TimerPageState();
+}
+
+class TimerPageState extends State<TimerPage> {
+  var _textStyleTimeLeftTextView =
+      TextStyle(fontSize: 18.0, color: Color(0xFFFF803A));
+  var _textStyleTimeLeftValue =
+      TextStyle(fontSize: 18.0, color: Color(0xFF000000));
+  var _countDownTextValue =
+      TextStyle(fontSize: 150.0, color: Color(0xFF717171));
+
   @override
   Widget build(BuildContext context) {
-    var currentColor = Colors.redAccent;
+    return Container(
+      child: Column(
+        children: <Widget>[_row1(), _row2(), _row3()],
+      ),
+    );
+  }
 
+  Widget _row1() {
+    return Container(
+        padding: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+        height: 100.0,
+        child: Row(
+          children: <Widget>[
+            Text(
+              "Time left: ",
+              style: _textStyleTimeLeftTextView,
+            ),
+            Text(
+              "100 secs",
+              style: _textStyleTimeLeftValue,
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment(1.0, 0.0),
+                child: Container(
+                  width: 60.0,
+                  height: 60.0,
+                  child: CircularGradientButton(
+                    child: Icon(
+                      Icons.pause,
+                      size: 25.0,
+                    ),
+                    callback: () {},
+                    gradient: Gradients.hotLinear,
+                  ),
+                ),
+              ),
+              flex: 1,
+            )
+          ],
+        ));
+  }
+
+  Widget _row2() {
+    return Center(
+        child: Text(
+      "10",
+      style: _countDownTextValue,
+    ));
+  }
+
+  Widget _row3() {
+    return Expanded(
+      child: MakeList(),
+    );
+  }
+}
+
+class MakeList extends StatefulWidget {
+  @override
+  MakeListState createState() => new MakeListState();
+}
+
+class MakeListState extends State<MakeList> {
+  var _rowHeight = 75.0;
+  var _textStyle = TextStyle(fontSize: 20.0, color: Color(0xFF000000));
+  var _indexStyle = TextStyle(fontSize: 20.0, color: Color(0xFFFEAF83));
+
+  var activities = [
+    "Prepare",
+    "Workout",
+    "Rest",
+    "Cycles",
+    "Rest",
+    "Prepare",
+    "FUCK",
+    "THIS",
+    "SHIT"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+//      padding: EdgeInsets.all(8.0),
+      itemBuilder: (context, i) {
+        if (i >= activities.length) {
+          return null;
+        }
+
+        return _singleRow(activities[i], i);
+      },
+    );
+  }
+
+  Widget _singleRow(String activity, int index) {
     return Material(
         child: Container(
-          height: _rowHeight,
-      padding: EdgeInsets.all(8.0),
+      height: _rowHeight,
       child: InkWell(
-          borderRadius: BorderRadius.all(Radius.circular(_circularRadius)),
-          splashColor: Colors.green,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.all(_sidePaddings),
-                  child: Icon(
-                    Icons.star,
-                    color: currentColor,
-                    size: 30.0,
-                  )
-//            child: Icon(Icons.star, color: Colors.red[500]),
+        highlightColor: Color(0xFFFEF0E8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      ((index + 1).toString() + "."),
+                      style: _indexStyle,
+                    )),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 16.0),
+                    child: Text(
+                      activity,
+                      style: _textStyle,
+                    )),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment(0.7, 0.0),
+                    child: Text(
+                      "10",
+                      style: TextStyle(fontSize: 22.0),
+                    ),
                   ),
-              Text(
-                "FIRST ROW",
-                style: _textStyle,
-              )
-            ],
-          )),
-      color: Colors.amber,
+                )
+              ],
+            ),
+          ],
+        ),
+        onTap: () {
+          setState(() {
+//            Scaffold.of(context).showSnackBar(new SnackBar(
+//              content: new Text("SHOWED"),
+//            ));
+          });
+        },
+      ),
+//          color: bgColor,
     ));
   }
 }
